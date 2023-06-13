0d9f3f2b184d77a8d4b19e5af6f3fc4526f6a90d
David Diaz <3006692160101@ingenieria.usac.edu.gt>
2023-06-12 18:27:17 -0600

    se creo api inicial
    Se creo la base de la api rest

---
+++ API-REST/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+app.set("port", process.env.PORT || 3000);
+app.set("json spaces", 2);
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//endpoint de prueba
+app.get("/", (req, res) => {
+  res.json({
+    Title: "Hola mundo",
+  });
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Server listening on port ${app.get("port")}`);
+});
