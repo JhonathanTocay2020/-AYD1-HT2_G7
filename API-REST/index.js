564314d166a725c29ac5d70802b8aea49dc908b4
David Diaz <3006692160101@ingenieria.usac.edu.gt>
2023-06-12 18:45:10 -0600

    se agrego el 4to endpoint
    Se realizo el 4toEndpoint de la api 201807420

--- API-REST/index.js
+++ API-REST/index.js
@@ -9,6 +9,34 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+function esNumeroPrimo(numero) {
+  if (numero <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(numero); i++) {
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//4to endpoint recibe numero y evalua que si es primo o no
+app.post("/primo-201807420", (req, res) => {
+  const numero = req.body.numero;
+  let respuesta = {};
+  if (esNumeroPrimo(numero)) {
+    respuesta = {
+      message: "si es numero Primo",
+    };
+  } else {
+    respuesta = {
+      message: "No es numero Primo",
+    };
+  }
+  res.json(respuesta);
+});
+
 //endpoint de prueba
 app.get("/", (req, res) => {
   res.json({
